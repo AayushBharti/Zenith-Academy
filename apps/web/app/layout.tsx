7303e2c6d9a2609c016b1e9ef9abef95e6a99758
AayushBharti <iaayushbharti@gmail.com>
2026-01-20 01:26:02 +0530

    feat: Introduce new UI components, refactor web app structure, update API configurations, and remove old ESLint and toast implementations.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,65 +1,65 @@
-import "./globals.css"
+import "./globals.css";
 
-import type { Metadata, Viewport } from "next"
-import { Inter } from "next/font/google"
-import localFont from "next/font/local"
-import { Toaster } from "react-hot-toast"
-
-import { cn } from "@/lib/utils"
-import Footer from "@/components/common/footer"
-import Navbar from "@/components/common/navbar/navbar"
-import { ThemeProvider } from "@/components/theme-provider"
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import Navbar from "@/components/common/navbar/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { cn } from "@/lib/utils";
+import BackendListener from "../components/common/backend-listner";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
-})
+});
 
 const geistMono = localFont({
   src: "../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-})
+});
 
 export const metadata: Metadata = {
   title: {
     default: "Zentih Minds",
-    template: `%s | Zentih Minds`,
+    template: "%s | Zentih Minds",
   },
   description:
     "Zentih Minds is a platform for learning and exploring the world of AI and technology.",
   icons: {
     icon: "/favicon.ico",
   },
-}
+};
 
 export const viewport: Viewport = {
   colorScheme: "dark",
   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "black" }],
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html className="dark" lang="en" suppressHydrationWarning>
       <head />
       <body
         className={cn(
-          "relative h-full antialiased mx-auto selection:bg-black selection:text-white dark:selection:bg-white dark:selection:text-black font-inter",
+          "relative mx-auto h-full font-inter antialiased selection:bg-black selection:text-white dark:selection:bg-white dark:selection:text-black",
           inter.variable,
           geistMono.variable
         )}
       >
         <ThemeProvider>
-          <Toaster />
+          <BackendListener />
+          <Toaster position="top-center" />
           <Navbar />
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
