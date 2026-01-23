99ee06efa2b468815d55c737216b74e8a09be6b5
AayushBharti <iaayushbharti@gmail.com>
2026-01-24 01:26:02 +0530

    feat: Introduce new UI components and refine existing ones, update project configuration, and enhance course management features.

--- apps/web/app/(course-view)/layout.tsx
+++ apps/web/app/(course-view)/layout.tsx
@@ -1,27 +1,20 @@
-import { useAuthStore } from "@/store/use-auth-store"
-import { useProfileStore } from "@/store/use-profile-store"
+import PrivateRoute from "@/components/auth/private-route";
+import { useAuthStore } from "@/store/use-auth-store";
+import { useProfileStore } from "@/store/use-profile-store";
 
-import PrivateRoute from "@/components/auth/private-route"
-
-import Loading from "../loading"
+import Loading from "../loading";
 
 export default function CourseViewLayout({
   children,
 }: {
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }) {
-  const { loading: authLoading } = useAuthStore.getState()
-  const { loading: profileLoading } = useProfileStore.getState()
+  const { loading: authLoading } = useAuthStore.getState();
+  const { loading: profileLoading } = useProfileStore.getState();
 
   if (authLoading || profileLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
-  return (
-    <PrivateRoute>
-      <div className="flex bg-background relative h-full container mx-auto antialiased">
-        {children}
-      </div>
-    </PrivateRoute>
-  )
+  return <PrivateRoute>{children}</PrivateRoute>;
 }
