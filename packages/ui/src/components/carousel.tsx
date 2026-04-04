9e4367a75618b175f58090a6e0193ad4f665a3a0
AayushBharti <iaayushbharti@gmail.com>
2026-04-04 21:55:55 +0530

    refactor: Move carousel into @workspace/ui; embla-backed carousel with navigation and orientation support

--- packages/ui/src/components/carousel.tsx
+++ packages/ui/src/components/carousel.tsx
@@ -5,8 +5,8 @@ import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react";
 import * as React from "react";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import { Button } from "./button";
+import { cn } from "../lib/utils";
 
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
@@ -67,35 +67,32 @@ const Carousel = React.forwardRef<
     const [canScrollPrev, setCanScrollPrev] = React.useState(false);
     const [canScrollNext, setCanScrollNext] = React.useState(false);
 
-    const onSelect = React.useCallback((api: CarouselApi) => {
+    const onSelect = (api: CarouselApi) => {
       if (!api) {
         return;
       }
 
       setCanScrollPrev(api.canScrollPrev());
       setCanScrollNext(api.canScrollNext());
-    }, []);
+    };
 
-    const scrollPrev = React.useCallback(() => {
+    const scrollPrev = () => {
       api?.scrollPrev();
-    }, [api]);
+    };
 
-    const scrollNext = React.useCallback(() => {
+    const scrollNext = () => {
       api?.scrollNext();
-    }, [api]);
+    };
 
-    const handleKeyDown = React.useCallback(
-      (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === "ArrowLeft") {
-          event.preventDefault();
-          scrollPrev();
-        } else if (event.key === "ArrowRight") {
-          event.preventDefault();
-          scrollNext();
-        }
-      },
-      [scrollPrev, scrollNext]
-    );
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+      if (event.key === "ArrowLeft") {
+        event.preventDefault();
+        scrollPrev();
+      } else if (event.key === "ArrowRight") {
+        event.preventDefault();
+        scrollNext();
+      }
+    };
 
     React.useEffect(() => {
       if (!(api && setApi)) {
