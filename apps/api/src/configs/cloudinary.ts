0b7f8f34748c5b56b7fd6e4bf4afd8bc63c88593
AayushBharti <iaayushbharti@gmail.com>
2026-05-24 22:23:41 +0530

    refactor: Move Cloudinary configuration into src/configs and read from validated env

---
+++ apps/api/src/configs/cloudinary.ts
@@ -0,0 +1,12 @@
+import { v2 } from "cloudinary";
+import env from "./env";
+
+v2.config({
+  cloud_name: env.CLOUDINARY_CLOUD_NAME,
+  api_key: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET,
+});
+
+const cloudinary = v2;
+
+export default cloudinary;
