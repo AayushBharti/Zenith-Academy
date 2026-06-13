dabfffe1763edcb5dafe93c2980a3fcbfd83692b
AayushBharti <iaayushbharti@gmail.com>
2026-06-13 22:14:35 +0530

    refactor: Move email verification mail template under src/mail

--- apps/api/src/mail/templates/email-verification-mail-template.ts
+++ apps/api/src/mail/templates/email-verification-mail-template.ts
@@ -66,18 +66,18 @@ export const otpTemplate = (otp: string) => `<!DOCTYPE html>
 	<body>
 		<div class="container">
 			<a href="https://academy.aayushbharti.in"><img class="logo"
-					src="" alt="Zenith Logo"></a>
+					src="" alt="Nextdemy Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
-				<p>Thank you for registering with Zenith. To complete your registration, please use the following OTP
+				<p>Thank you for registering with Nextdemy. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
 				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
 				Once your account is verified, you will have access to our platform and its features.</p>
 			</div>
 			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-					href="mailto:academy@aayushbharti.in">info@Zenith.com</a>. We are here to help!</div>
+					href="mailto:academy@aayushbharti.in">info@Nextdemy.com</a>. We are here to help!</div>
 		</div>
 	</body>
 	
