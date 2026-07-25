6e6dc9653a3192e084f1542f9150c2c6ed1054e2
AayushBharti <iaayushbharti@gmail.com>
2026-07-25 23:31:00 +0530

    refactor: Drop local category, course and profile types in favour of @workspace/shared-types

--- apps/web/types/category.ts
+++
@@ -1,9 +0,0 @@
-import type { CourseDetails } from "./course";
-
-export interface Category {
-  _id: string;
-  name: string;
-  description: string;
-  slug: string;
-  courses: CourseDetails[];
-}
